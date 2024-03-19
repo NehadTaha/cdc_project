689b0eb8cc5ecaede3336182efb301c9a88c8e95
Nehad Taha <le2125697@crc-lennox.qc.ca>
2024-03-19 01:24:06 -0400

    Remove unused files and update dependencies

---
+++ capacitor.config.js
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config = {
+    appId: "com.example.app",
+    appName: "RÉPERTOIRE DES ORGANISMES",
+    webDir: "build",
+    server: {
+        androidScheme: "https"
+    }
+};
+
+export default config;
