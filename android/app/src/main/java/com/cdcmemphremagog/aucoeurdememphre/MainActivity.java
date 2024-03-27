244ccbae620737eef3d323d3e5b40058ff8238fe
Nehad Taha <le2125697@crc-lennox.qc.ca>
2024-03-27 16:55:11 -0400

    Add android&ios

---
+++ android/app/src/main/java/com/cdcmemphremagog/aucoeurdememphre/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cdcmemphremagog.aucoeurdememphre;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cdcmemphremagog.aucoeurdememphre',
+  appName: 'Au coeur de Memphré',
+  webDir: 'build',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
